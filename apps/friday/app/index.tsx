4e67af78e028e8dc7c5373a0c6e21222aa4f68a9
kvqn <guneetaggarwal@gmail.com>
2024-07-23 07:29:55 +0530

    added drawer

--- apps/friday/app/index.tsx
+++ apps/friday/app/index.tsx
@@ -1,15 +1,16 @@
+import { AddNewServerEndpoint } from "@/components/screens/add-new"
+import { createDrawerNavigator } from "@react-navigation/drawer"
+import { NavigationContainer } from "@react-navigation/native"
 import { Text, View } from "react-native"
+
+const Drawer = createDrawerNavigator()
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <NavigationContainer independent={true}>
+      <Drawer.Navigator initialRouteName="Add New Server">
+        <Drawer.Screen name="Add New Server" component={AddNewServerEndpoint} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   )
 }
---
+++ apps/friday/components/screens/add-new.tsx
@@ -0,0 +1,10 @@
+import { Text, TextInput, View } from "react-native"
+
+export function AddNewServerEndpoint() {
+  return (
+    <View>
+      <Text>Add new server endpoint</Text>
+      <TextInput />
+    </View>
+  )
+}
