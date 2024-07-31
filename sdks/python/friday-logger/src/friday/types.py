b490fb6d9930b398fe5179b95b66a2ded8f761bb
kvqn <guneetaggarwal@gmail.com>
2024-07-31 19:32:49 +0530

    added aggregator

---
+++ sdks/python/friday-logger/src/friday/types.py
@@ -0,0 +1,31 @@
+from typing import TypedDict, List, Optional
+from datetime import datetime
+from enum import Enum
+
+class NamespaceAndTopics(TypedDict):
+    namespace: str
+    topic: str
+
+class LevelEnum(Enum):
+    DEBUG = "DEBUG"
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+    CRITICAL = "CRITICAL"
+
+class QueryInput(TypedDict):
+    namespace_and_topics: List[NamespaceAndTopics]
+    level: Optional[LevelEnum]
+    before: Optional[datetime]
+    after: Optional[datetime]
+    limit: Optional[int]
+
+class FridayLogRecord(TypedDict):
+    id: int
+    namespace: str
+    topic: str
+    level: LevelEnum
+    data: str
+    timestamp: datetime
+
+DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
