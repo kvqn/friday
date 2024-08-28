d85c90b3e6c1588cef3d3f7ad35e2d844bab872d
kvqn <guneetaggarwal@gmail.com>
2024-08-29 02:57:34 +0530

    chore: removing the old api and replacing with new api

--- api/drizzle/0000_initial.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `logs` (
-	`id` serial NOT NULL,
-	`namespace` char(128) NOT NULL DEFAULT 'default',
-	`topic` char(128) NOT NULL DEFAULT 'default',
-	`level` enum('DEBUG','INFO','WARNING','ERROR','CRITICAL') NOT NULL DEFAULT 'INFO',
-	`data` text,
-	`timestamp` timestamp NOT NULL DEFAULT (now()),
-	CONSTRAINT `logs_id` PRIMARY KEY(`id`)
-);
--- api/src/db/migrate.ts
+++
@@ -1,8 +0,0 @@
-import { migrate } from "drizzle-orm/mysql2/migrator"
-import { db, connection } from "./conn"
-
-// This will run migrations on the database, skipping the ones already applied
-await migrate(db, { migrationsFolder: "./drizzle" })
-
-// Don't forget to close the connection, otherwise the script will hang
-await connection.end()
