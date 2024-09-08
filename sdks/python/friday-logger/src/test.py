13241e9c1c6c2db1a4a806d644f450fa8dc13d64
kvqn <guneetaggarwal@gmail.com>
2024-09-08 14:18:08 +0530

    feat: opinionated stream handler

--- sdks/python/friday-logger/src/test.py
+++ sdks/python/friday-logger/src/test.py
@@ -1,16 +1,17 @@
 from friday import Logger, Aggregator
 
-ENDPOINT = "http://localhost:5001"
+ENDPOINT = "http://localhost:5000"
 # ENDPOINT = "https://friday-api.guneet-homelab.duckdns.org"
 logger = Logger(
     "test-logger",
     ENDPOINT,
     "test",
     "test",
+    use_opinionated_stream_handler=True
 )
 
 logger.debug("This is a debug message")
 
 aggregator = Aggregator(ENDPOINT)
 
-print(aggregator.query(limit=2, namespaces=["test"], topics=["test"]))
+print(aggregator.query(limit=2, namespace="test", topics=["test"]))
